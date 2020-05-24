ff4cde79c2cbfb6da7630bf1fa1df278f928561f
Kush <kushv2660@gmail.com>
2020-05-24 17:43:06 +0530

    first build

---
+++ precache-manifest.7c6d566a6c02f8b399c0cb1469ca4c84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b04a14a562ec5c8f96c30455cbac27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26592eb8c6b5977b1ae3",
+    "url": "/static/css/main.6b39bd57.chunk.css"
+  },
+  {
+    "revision": "2ecf75edd1df1b19a1b1",
+    "url": "/static/js/2.0ec06b89.chunk.js"
+  },
+  {
+    "revision": "e9c1b7c4086e9f56b74580268d33baea",
+    "url": "/static/js/2.0ec06b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26592eb8c6b5977b1ae3",
+    "url": "/static/js/main.05bd1119.chunk.js"
+  },
+  {
+    "revision": "8c31587c25d008a2b8bf",
+    "url": "/static/js/runtime-main.7d63a2cf.js"
+  },
+  {
+    "revision": "d316832c053d1fc67249f56e284d3d0f",
+    "url": "/static/media/ico.d316832c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
